aae58bc50a945113e140901818246d90f8c54770
bhoomika1313-ally <bhoomikaa1313@gmail.com>
2026-02-24 10:27:48 +0530

    hey

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Hello World!");
+/*console.log("Hello World!");
 var num = 10;
 console.log(num);
 console.log(typeof(num));
@@ -23,3 +23,28 @@ if (a === b) {
 } else {
     console.log("a and b are different");
 } 
+num = 10;
+var nil = 20;
+(num==8&&nil>1) ? console.log("Both num and nil are truthy") : console.log("Either num or nil is falsy");
+(num == 5||nil == 2) ? console.log("Either num is 5 or nil is 2") : console.log("Neither num is 5 nor nil is 20");
+
+//var votable = (age<18) ? "Too young to vote" : "Old enough to vote";
+ var c = 5;
+ var d = 10;
+ let result = c+d;
+ console.log(result);
+ let result2 = c-d;
+console.log(result2);
+let result3 = c*d;
+console.log(result3);
+let result4 = c/d;
+console.log(result4);
+let result5 = c%d;
+console.log(result5);*/
+
+var count;
+document.write("starting loop" + "<br/>");
+for(count = 0; count < 10; count++){
+    document.write("current count: " + count);
+    document.write("<br/>");}
+    document.write("loop ended");
