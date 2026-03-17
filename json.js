3e286ecb6cdaaf05de5d025fc2b4319f4b27c948
bhoomika1313-ally <bhoomikaa1313@gmail.com>
2026-03-17 10:30:13 +0530

    j

--- json.js
+++ json.js
@@ -3,3 +3,14 @@ obj = JSON.parse(text);
 console.log(obj);
 console.log(obj.employees[0].firstName);
 console.log(obj.employees[1].firstName);
+
+// arrow is an higher order function
+// and it can be passed as an argument to another function
+// and it can be returned by another function
+
+const add = (x,y) => x+y; 
+const subtract = (x,y) => x-y;
+const multiply = (x,y) => x*y;
+const divide = (x,y) => x/y;
+
+// if only 1 line in arrow function then we can omit the curly braces and the return keyword
